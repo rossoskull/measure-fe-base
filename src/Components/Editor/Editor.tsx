0e28d71cc1c26e5cb57b087f9bb23c5f23875845
Jay Mistry <mistry.jay15@gmail.com>
2021-07-06 03:23:54 +0530

    More cleanup :wastebasket:

---
+++ src/Components/Editor/Editor.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Editor = () => {
+  return (
+    <div className="editor">
+      <h1>New Editor</h1>
+    </div>
+  )
+}
+
+export default Editor
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Editor from './Components/Editor/Editor'
+
+import './Sass/_base.scss'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Editor />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
